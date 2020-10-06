7ff872e74a0bbf982d6ec5355965c04cbecdcfae
Nicolas Mopin <Nicolas.mopin@edu.devinci.fr>
2020-10-07 00:33:27 +0200

    create file

---
+++ model/product-model.php
@@ -0,0 +1,11 @@
+<?php
+
+function dbconnect(){
+    try{
+        $db = new PDO('mysql:host=localhost;dbname=back_office', 'root', 'root', array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_WARNING));
+        return $db;
+    }
+    catch(Exception $e){
+        die('Erreur : '.$e->getMessage());
+    }
+}
