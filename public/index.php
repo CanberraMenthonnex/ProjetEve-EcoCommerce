a41080e04d8ecc1c93bc84c8c5d35f6731ad7097
Mathieu <mathieu.raimbault@hotmail.com>
2020-10-06 19:27:13 +0200

    :art: Add public folder and index.php inside of it

---
+++ public/index.php
@@ -0,0 +1,41 @@
+<?php 
+require ("../configuration/configuration.php");
+require("../routes/router/Route.php");
+require("../routes/router/DynamicRoute.php");
+require("../routes/router/Router.php");
+
+require("../controllers/user-controller.php");
+
+
+
+try {
+
+    $router = new Router\Router($_GET["url"]);
+
+    $router->get("users/login", function () {
+        logPage();
+    });
+
+    $router->post("users/login", function () {
+        login();
+    });
+
+    $router->get("users/signin", function () {
+        subPage();
+    });
+
+    $router->post("users/signin", function () {
+        signIn();
+    });
+
+
+
+    $router->parse();
+
+} catch (Exception $e) {
+    echo "<strong>ERROR : </strong>";
+    die($e->getMessage());
+
+}
+
+
