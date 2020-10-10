da9193f91a0bd866e27dea080c923f039c9186f8
Armand <armandorard@live.fr>
2020-10-10 15:05:07 +0200

    :sparkles: ajout header de l'index; css; et quelques img

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ require("../routes/router/Route.php");
 require("../routes/router/DynamicRoute.php");
 require("../routes/router/Router.php");
 
-require("../controllers/user-controller.php");
+//require("../controllers/user-controller.php");
 
 
 
@@ -12,20 +12,24 @@ try {
 
     $router = new Router\Router($_GET["url"]);
 
+    $router->get("", function () {
+        require("../views/index.php");
+    });
+
     $router->get("users/login", function () {
-        logPage();
+        //logPage();
     });
 
     $router->post("users/login", function () {
-        login();
+        //login();
     });
 
     $router->get("users/signin", function () {
-        subPage();
+        //subPage();
     });
 
     $router->post("users/signin", function () {
-        signIn();
+        //signIn();
     });
 
 
---
+++ views/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= MAIN_PATH ?>style.css">
+
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div id="boutonHeader">
+            <p>item1</p>
+            <p>item2</p>
+            <p>item3</p>
+        </div>
+        <a href=""><div id="divLogo">
+            <img id="logoImg" src="img/logo.png" alt="logo du site">
+        </div></a>
+        <div id="divPanier">
+            <img id="panierImg" src="img/panier.png" alt="panier">
+        </div>
+    </header>
+
+    <footer>
+        <img class="iconeF" src="img/insta.png" alt="instagram">
+        <img class="iconeF" src="img/what.png" alt="what's app">
+        <img class="iconeF" src="img/facebook.png" alt="facebook">
+    </footer>
+</body>
+</html>
