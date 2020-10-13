1836393dca8bbf09d8b16893494836c1968a0eb6
Armand <armandorard@live.fr>
2020-10-13 20:39:48 +0200

    :sparkles: Add slider to index and JS file

---
+++ public/main.js
@@ -0,0 +1,11 @@
+var slide = ["slide1.jpg","slide2.jpg","slide3.jpg"];
+var numero = 0;
+
+function ChangeSlide(sens) {
+    numero = numero + sens;
+    if (numero < 0)
+        numero = slide.length - 1;
+    if (numero > slide.length - 1)
+        numero = 0;
+    document.getElementById("slide").src = "img/" + slide[numero];
+}
--- views/index.php
+++ views/index.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= MAIN_PATH ?>style.css">
-
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/97b40379bf.js" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
@@ -22,10 +22,21 @@
         </div>
     </header>
 
+    <div id="searchs">
+        <i class="fas fa-search"></i><input id="search" type="text">
+    </div>
+    <div id="slider">
+        <img src="img/slide1.jpg" alt="Photo" id="slide">
+        <div id="precedent" onclick="ChangeSlide(-1)"><</div>
+        <div id="suivant" onclick="ChangeSlide(1)">></div>
+    </div>
+
     <footer>
         <img class="iconeF" src="img/insta.png" alt="instagram">
         <img class="iconeF" src="img/what.png" alt="what's app">
         <img class="iconeF" src="img/facebook.png" alt="facebook">
     </footer>
+
+<script src="main.js"></script>
 </body>
 </html>
