bf814d2974527901c7862342049a64def33fb69c
Canberra Menthonnex <Canberra.menthonnex@edu.devinci.fr>
2020-10-16 22:21:33 +0200

    :sparkels: add admin check

--- configuration/dbconfiguration.php
+++ configuration/dbconfiguration.php
@@ -2,4 +2,4 @@
 define("DB_HOST", "localhost");
 define("DB_NAME", "back_office");
 define("DB_USERNAME", "root");
-define("DB_PASSWORD", "");
+define("DB_PASSWORD", "root");
--- models/product-model.php
+++ models/product-model.php
@@ -2,7 +2,7 @@
 
 function dbconnect(){
     try{
-        $db = new PDO('mysql:host=localhost;dbname=back_office', 'root', '', array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_WARNING));
+        $db = new PDO('mysql:host=localhost;dbname=back_office', 'root', 'root', array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_WARNING));
         return $db;
     }
     catch(Exception $e){
