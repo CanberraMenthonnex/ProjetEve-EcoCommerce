d225b84ee3da402d37cb1d2c832859a04e12d106
Canberra Menthonnex <Canberra.menthonnex@edu.devinci.fr>
2020-11-04 22:21:58 +0100

    switch branch

--- models/Model.php
+++ models/Model.php
@@ -11,7 +11,7 @@ abstract class Model {
      * */
     private static function getDb() : \PDO{
         return new \PDO(
-            'mysql:host='. DB_HOST . ';dbname=' . DB_NAME , DB_USERNAME , DB_PASSWORD,
+            'mysql:host='. DB_HOST . ';dbname=' . DB_NAME , DB_USERNAME , DB_PASSWORD, 
             array(\PDO::ATTR_ERRMODE=>\PDO::ERRMODE_WARNING,\PDO::ATTR_DEFAULT_FETCH_MODE=>\PDO::FETCH_OBJ)
         );
     }
