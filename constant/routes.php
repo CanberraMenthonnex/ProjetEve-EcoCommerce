8c771cb2b92aa52e016e7d5d7e83405e25987190
Canberra Menthonnex <Canberra.menthonnex@edu.devinci.fr>
2020-11-15 14:04:12 +0100

    commit for pull

--- constant/routes.php
+++ constant/routes.php
@@ -1,8 +1,17 @@
 <?php
+
+//Admin//
 const ADMIN_LOG_ROUTE = "/admin/login";
 const ADMIN_LOGOUT_ROUTE = "/admin/logout";
 const ADMIN_CREATE_PRODUCT_ROUTE = "/admin/product/form";
 const ADMIN_GET_PRODUCT_ROUTE = "/admin/product/stock";
 const ADMIN_DELETE_PRODUCT_ROUTE = "/admin/product/";
-const CUSTOMER_GET_SIGN_ROUTE = "/customer/sign";
+
+
+//Customer//
 const HOME_ROUTE = "/";
+const CUSTOMER_GET_SIGN_ROUTE = "/customer/sign";
+
+
+
+
