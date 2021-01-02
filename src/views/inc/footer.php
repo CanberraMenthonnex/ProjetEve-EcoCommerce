913bf39d728e1a6c746dde855b8d57b9eab9be21
mathieu raimbault <mathieu.raimbault@hotmail.com>
2021-01-03 00:13:13 +0100

    :sparkles: Add responsive header

--- src/views/inc/footer.php
+++ src/views/inc/footer.php
@@ -1,39 +1,39 @@
-<footer class="footer">
+<footer class="classic-footer">
 
-    <img src="<?= MAIN_PATH ?>/img/logo.png" alt="Eve logo" title="Projet Eve" class="footer--logo">
+    <img src="<?= MAIN_PATH ?>/img/logo.png" alt="Eve logo" title="Projet Eve" class="classic-footer--logo">
     <ul>
-        <li>
-            <a href="#" class="f-white">Notre histoire</a>
+        <li class="classic-footer--item">
+            <a href="#" class="classic-footer--item">Notre histoire</a>
         </li>
-        <li>
-            <a href="#" class="f-white">Contact</a>
+        <li class="classic-footer--item">
+            <a href="#" class="classic-footer--item">Contact</a>
         </li>
-        <li>
-            <a href="#" class="f-white">Conditions</a>
+        <li class="classic-footer--item">
+            <a href="#" class="classic-footer--item">Conditions</a>
         </li>
 
     </ul>
     <ul>
-        <li>
-            <a href="#" class="f-white">
+        <li class="classic-footer--item">
+            <a href="#" class="classic-footer--item">
                 <img src="<?= MAIN_PATH ?>/img/facebook.svg" alt="facebook" title="facebook">
                 Facebook
              </a>
         </li>
-        <li>
-            <a href="#" class="f-white">
+        <li class="classic-footer--item">
+            <a href="#" class="classic-footer--item">
                 <img src="<?= MAIN_PATH ?>/img/twitter.svg" alt="facebook" title="facebook">
                 Twitter
             </a>
         </li>
-        <li>
-            <a href="#" class="f-white">
+        <li class="classic-footer--item">
+            <a href="#" class="classic-footer--item">
                 <img src="<?= MAIN_PATH ?>/img/instagram.svg" alt="facebook" title="facebook">
                 Instagram
             </a>   
         </li>
     </ul>
-    <address class="f-white">
+    <address class="classic-footer--adress">
         26 rue de de la galaxy, la Lune <br>
         000 000 000 <br>
         projet-eve@eco-commerce.com
