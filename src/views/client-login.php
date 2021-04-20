cebd720001261bd8e1c1a91c25cf0762db0b14c0
Slb <sacha.le_bras@edu.devinci.fr>
2021-04-20 17:10:28 +0200

    Add scss login/ sign in

--- src/views/client-login.php
+++ src/views/client-login.php
@@ -11,8 +11,8 @@ use Core\Router\PathGenerator;
 <article id="log_sign" class=" <?php if(isset($that_fuking_error)){echo "form_log_error_sign";}?>">
             
             <form action="<?= MAIN_PATH ?>/customer/login" method ="POST"  id="log">
-                <div class="flex--column justify--center align--center fixeMargin3" id="form_login" class="form2" >
-                    <h2 class="white bold py-1">Connexion</h2>
+                <div class="bg--flash-green flex--column justify--center align--center fixeMargin3 width40 marginTop5 marginAuto"  id="form_login" class="form2" >
+                    <h2 class="f-white bold py-1">Connexion</h2>
                     <span class='error' >
                             <?php 
                                 if(isset($log_error)){
@@ -20,10 +20,10 @@ use Core\Router\PathGenerator;
                                 }
                             ?>
                         </span>
-                    <input class="bold px20 semiRadius" id="username" placeholder="Adresse Email" name="username" type="text" >
-                    <input class="my-1 semiRadius" class="bold px20" id="pwd"  placeholder="Mot de passe" name="pwd" type="password" >
-                    <button class="semiRadius paddingCta" type="submit">Connexion</button>
-                    <p class="py-1" class="white px17">Pas encore de compte ? </p>
+                    <input class="text-input2 marginTop1" id="username" placeholder="Adresse Email" name="username" type="text" >
+                    <input class="text-input2  marginTop1" class="bold px20" id="pwd"  placeholder="Mot de passe" name="pwd" type="password" >
+                    <button class="my-1 semiRadius paddingCta bg--white" type="submit">Connexion</button>
+                    <p class="py-1 f-white" class="white px17">Pas encore de compte ? </p>
                     <a class="white bold px20 noDecoration" id="logToSign" href="<?= PathGenerator::generatePath(CUSTOMER_POST_SIGN_ROUTE) ?> ">Rejoins-nous !</a>
                 </div>
             </form>
