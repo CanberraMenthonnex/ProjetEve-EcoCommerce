5480f9f81ac9d2290343ef1953e6dee0ae582ee2
Canberra Menthonnex <Canberra.menthonnex@edu.devinci.fr>
2021-04-20 17:33:56 +0200

    git for pull

--- src/views/inc/footer.php
+++ src/views/inc/footer.php
@@ -39,10 +39,3 @@
         projet-eve@eco-commerce.com
     </address>
 </footer>
-<<<<<<< HEAD
-<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-<script src="<?= MAIN_PATH ?>/js/header.js"></script>
-<script>const MAIN_PATH = <?= json_encode(MAIN_PATH) ?></script>
-<script src="<?= MAIN_PATH ?>/js/ajax-cart.js"></script>
-=======
->>>>>>> 7123258d3323a4cabefb53870665f6dcebcbed49
