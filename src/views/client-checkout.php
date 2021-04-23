5cfd0dfa8d23bafae88d20abdcc98a0bb342282b
Canberra Menthonnex <Canberra.menthonnex@edu.devinci.fr>
2021-04-23 11:46:54 +0200

    add checkout and style

--- src/views/client-checkout.php
+++ src/views/client-checkout.php
@@ -3,29 +3,68 @@
 use Controller\CartController;
 use Core\View\Template\Template;
 
+ob_start()
+
 ?>
 
-<h1>Détails de la commande</h1>
+<div class="checkout-content">
+    <h1>Détails de la commande</h1>
 
-<div>
-    <table>
-        <?php
-            $t = "test";
-            
-            echo $t;
+    <div>
         
-        ?>
-    </table>
+            <table class="checkout-table">
+                <thead>
+                    <tr>
+                        <th colspan="2">Nom du produit</th>
+                        <th colspan="2">Quantité</th>
+                        <th colspan="2">Prix total</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php 
+                    foreach($cartList as $product){
+                ?>
+                
+                    <tr>
+                        <td colspan="2"><?= $product["name"] ?></td>
+                        <td colspan="2"><?= $product["quantity"] ?></td>
+                        <td colspan="2"><?= $product["price"] ?></td>
+                    </tr>
+                    
+                
+                <?php
+                    }
+                ?>
+                
+                </tbody>
+                
+                <tfoot>
+                    <tr>
+                        <td colspan="2" class="line-cell"></td>
+                        <td colspan="2" class="line-cell"></td>
+                        <td colspan="2" class="line-cell"></td>
+                    </tr>    
 
+                    <tr>
+                        <td colspan="2"><strong>Total à payer :</strong></td>
+                        <td colspan="2" class="last-row"> <?= $totalProduct ?> </td>
+                        <td colspan="2" class="last-row"> <?= $totalPrice  . " €"?> </td>        
+                    </tr>
+                </tfoot>
+            </table>
+
+
+            
+
+    </div>
 </div>
-
 
 <?php
 
 
-$cart = new CartController();
 
-// echo $cart;
+
+
 
 $content = ob_get_clean();
 $temp = new Template("Checkout", [], ["index"]);
